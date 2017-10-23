29b13344795c7200218a7ea8fe8a3cc16ed5cdaf
Yoga Hapriana <thenelse@rocketmail.com>
2017-10-23 10:52:38 +0700

    Class-Based Components - Section 1 - Lecture 15

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+import SearchBar from './components/search_bar';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const API_KEY = 'AIzaSyDI7t7t1DnLxnYsKb9cjTAT0tXFmoJxkus';
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// Create a new component. This component should produce
+// some HTML
+const App = () => {
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
+}
+
+// Take this component's generated HTML and put it
+// on the page (in the DOME)
+ReactDOM.render(<App />, document.querySelector('.container'));
