edd1bf566a6d7ed95cd6be679a70d04c74fc57d5
Yoga Hapriana <thenelse@rocketmail.com>
2017-11-07 17:13:27 +0700

    Section 2 - Lecture 23 - Props

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 const API_KEY = 'AIzaSyCt9ZhR2ufdab7Cc0Uak_rcT7goGQVbKT4';
 
 // Create a new component. This component should produce
@@ -21,6 +22,7 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
+        <VideoList videos={ this.state.videos } />
       </div>
     );
   }
