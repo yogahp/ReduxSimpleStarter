bf666163075022ea85fe69654bf2a9ecaa40ba64
Yoga Hapriana <thenelse@rocketmail.com>
2017-11-10 13:35:24 +0700

    Section 5 - Lecture 58 - Redux-Promise in Practice

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = '64e3844a1880a96eed7562b7a06e7de3';
-const ROOT_RUL = `http://api.openweathermap.org/data/2.5/forecast?appid=${ API_KEY }`;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${ API_KEY }`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
