d2365df0d718606158bca4610ba50f8d0a9ad03a
Yoga Hapriana <thenelse@rocketmail.com>
2017-11-13 10:53:23 +0700

    Section 5 - Lecture 60 - Avoiding State Mutations in Reducers

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,3 +1,9 @@
+import { FETCH_WEATHER } from '../actions/index';
+
 export default function(state = null, action) {
+  switch (action.type) {
+    case FETCH_WEATHER:
+      return [ action.payload.data, ...state ];
+  }
   return state;
 }
